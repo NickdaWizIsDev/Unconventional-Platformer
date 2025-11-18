0a82fdace8867f8ea7d89920c7da58331a028d80
NickdaWizIsDev <nicocastrito2005@gmail.com>
2025-11-18 12:14:42 -0300

    a

--- Assets/Scripts/AnimatorManager.cs
+++ Assets/Scripts/AnimatorManager.cs
@@ -5,13 +5,15 @@ public class AnimatorManager : MonoBehaviour
     [SerializeField] Animator animator;
     public bool IsMoving { get { return IsMoving; } set { animator.SetBool("IsMoving", value); } }
     public bool CanMove { get { return animator.GetBool("CanMove"); } }
-    public float XVelocity { get { return XVelocity; } set { XVelocity = value; } }
+    public float XVelocity { get { return XVelocity; } set { animator.SetFloat("xVelocity", value); } }
 
     // Update is called once per frame
     void Update()
     {
         if (XVelocity < 0) transform.localScale = new(-1, 1, 1);
         else transform.localScale = Vector3.one;
+
+        IsMoving = XVelocity != 0;
     }
     
     public void PlayAnimation(AnimationClip clip)
